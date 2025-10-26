4ccfdf17315670f4fd209ca358b9ca9d95a221ea
Berk Ulusoy <berkuluusoy@gmail.com>
2025-10-26 23:11:13 +0300

    github için

--- BMSV2UIDEMO/vite.config.js
+++ BMSV2UIDEMO/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/meter-management-demo/BMSV2UIDEMO/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
